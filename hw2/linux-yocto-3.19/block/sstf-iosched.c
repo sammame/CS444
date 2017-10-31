2890fda0133032ceef642438850db5fe964dd813
Drake Seifert <seifertd@os2.engr.oregonstate.edu>
2017-10-30 17:44:04 -0700

    Adjustments in sstf algorithm

--- hw2/linux-yocto-3.19/block/sstf-iosched.c
+++ hw2/linux-yocto-3.19/block/sstf-iosched.c
@@ -1,5 +1,5 @@
 /*
- * elevator clook
+ * elevator noop
  */
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
@@ -8,41 +8,72 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
-struct clook_data {
+struct sstf_data {
 	struct list_head queue;
 };
 
-static void clook_merged_requests(struct request_queue *q, struct request *rq,
+static sector_t prev_sector;
+
+static void sstf_merged_requests(struct request_queue *q, struct request *rq,
 				 struct request *next)
 {
 	list_del_init(&next->queuelist);
 }
 
-static int clook_dispatch(struct request_queue *q, int force)
+//Here is where the LOOK algorithm is implemented
+static int sstf_dispatch(struct request_queue *q, int force)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
+	struct sstf_data *nd = q->elevator->elevator_data;
+	struct request *prev_rq = NULL;
+	int temp = -1;
+	int prev = -1;
+	sector_t sec;
 
 	if (!list_empty(&nd->queue)) {
 		struct request *rq;
-		rq = list_entry(nd->queue.next, struct request, queuelist);
-		list_del_init(&rq->queuelist);
-		elv_dispatch_sort(q, rq);
+			list_for_each_entry(rq, &nd->queue, queuelist) {
+				if (rq == 0) {
+					printk("Error: Queue empty :(\n");
+					return 0;
+				}
+				//Determine direction
+				sec = blk_rq_pos(rq);
+        			if (prev_sector >= sec) {
+        				temp = prev_sector - sec;
+        			} else {
+        				temp = sec - prev_sector;
+       				}
+		        	sec = blk_rq_pos(rq);
+				if (prev > temp || prev == -1) {
+					prev = temp;
+					prev_rq = rq;
+				}
+			}
+		prev_sector = blk_rq_sectors(prev_rq) + blk_rq_pos(prev_rq);
+		//Merge
+                list_del_init(&prev_rq->queuelist);
+                elv_dispatch_add_tail(q, prev_rq); 
+
+		printk("Popping request at sector position: %llu\n",
+					(unsigned long long)blk_rq_pos(prev_rq));
 		return 1;
 	}
 	return 0;
 }
 
-static void clook_add_request(struct request_queue *q, struct request *rq)
+static void sstf_add_request(struct request_queue *q, struct request *rq)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
+	struct sstf_data *nd = q->elevator->elevator_data;
+	printk("Adding request at sector position: %llu\n",
+			(unsigned long long)blk_rq_pos(rq));
 
 	list_add_tail(&rq->queuelist, &nd->queue);
 }
 
 static struct request *
-clook_former_request(struct request_queue *q, struct request *rq)
+sstf_former_request(struct request_queue *q, struct request *rq)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
+	struct sstf_data *nd = q->elevator->elevator_data;
 
 	if (rq->queuelist.prev == &nd->queue)
 		return NULL;
@@ -50,18 +81,18 @@ clook_former_request(struct request_queue *q, struct request *rq)
 }
 
 static struct request *
-clook_latter_request(struct request_queue *q, struct request *rq)
+sstf_latter_request(struct request_queue *q, struct request *rq)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
+	struct sstf_data *nd = q->elevator->elevator_data;
 
 	if (rq->queuelist.next == &nd->queue)
 		return NULL;
 	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-static int clook_init_queue(struct request_queue *q, struct elevator_type *e)
+static int sstf_init_queue(struct request_queue *q, struct elevator_type *e)
 {
-	struct clook_data *nd;
+	struct sstf_data *nd;
 	struct elevator_queue *eq;
 
 	eq = elevator_alloc(q, e);
@@ -83,42 +114,42 @@ static int clook_init_queue(struct request_queue *q, struct elevator_type *e)
 	return 0;
 }
 
-static void clook_exit_queue(struct elevator_queue *e)
+static void sstf_exit_queue(struct elevator_queue *e)
 {
-	struct clook_data *nd = e->elevator_data;
+	struct sstf_data *nd = e->elevator_data;
 
 	BUG_ON(!list_empty(&nd->queue));
 	kfree(nd);
 }
 
-static struct elevator_type elevator_clook = {
+static struct elevator_type elevator_sstf = {
 	.ops = {
-		.elevator_merge_req_fn		= clook_merged_requests,
-		.elevator_dispatch_fn		= clook_dispatch,
-		.elevator_add_req_fn		= clook_add_request,
-		.elevator_former_req_fn		= clook_former_request,
-		.elevator_latter_req_fn		= clook_latter_request,
-		.elevator_init_fn		= clook_init_queue,
-		.elevator_exit_fn		= clook_exit_queue,
+		.elevator_merge_req_fn		= sstf_merged_requests,
+		.elevator_dispatch_fn		= sstf_dispatch,
+		.elevator_add_req_fn		= sstf_add_request,
+		.elevator_former_req_fn		= sstf_former_request,
+		.elevator_latter_req_fn		= sstf_latter_request,
+		.elevator_init_fn		= sstf_init_queue,
+		.elevator_exit_fn		= sstf_exit_queue,
 	},
-	.elevator_name = "clook",
+	.elevator_name = "sstf",
 	.elevator_owner = THIS_MODULE,
 };
 
-static int __init clook_init(void)
+static int __init sstf_init(void)
 {
-	return elv_register(&elevator_clook);
+	return elv_register(&elevator_sstf);
 }
 
-static void __exit clook_exit(void)
+static void __exit sstf_exit(void)
 {
-	elv_unregister(&elevator_clook);
+	elv_unregister(&elevator_sstf);
 }
 
-module_init(clook_init);
-module_exit(clook_exit);
+module_init(sstf_init);
+module_exit(sstf_exit);
 
 
-MODULE_AUTHOR("Jens Axboe");
+MODULE_AUTHOR("Austin Brown - 13");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("C-look IO scheduler");
+MODULE_DESCRIPTION("SSTF IO scheduler");
