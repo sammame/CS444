43aef347fe1fb4380192289dcddf5554c38d1e46
Drake Seifert <seifertd@os2.engr.oregonstate.edu>
2017-11-09 14:25:12 -0800

    Adding the last few hw2 files

---
+++ hw2/READ_ME.txt
@@ -0,0 +1,3 @@
+To compile the tex file, run "make" twice.
+To create create our patch file we used diff only on the files that were manipulated
+in the block directory.
---
+++ hw2/rwscript.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import os
+
+# Create, read to, write to, and delete files ....
+#1
+file = open("testfile.txt","w") 
+file.write("Hello World. ")
+file.write("This is a test file.")
+file.close()
+
+#2
+file = open("testfile2.txt","w") 
+file.write("Hello World again. ")
+file.write("This is another test file.")
+file.close()
+
+#3
+file = open("testfile.txt","r") 
+print file.read() 
+file.close
+
+
+#4
+file = open("testfile3.txt","w") 
+file.write("Hello World for the third time. ")
+file.write("I AM THE LAST TEST FILE.")
+file.close()
+
+#4
+file = open("testfile2.txt","r") 
+print file.read() 
+file.close
+
+#5
+file = open("testfile3.txt","r") 
+print file.read() 
+file.close
+
+
+#Clean up all files created 
+os.remove("testfile.txt")
+os.remove("testfile2.txt")
+os.remove("testfile3.txt")
